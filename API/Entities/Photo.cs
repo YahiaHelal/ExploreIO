0449031d49a3d69fda6b3e2b96dc03e1db899fe5
YahiaHelal <yahiarashwan348@gmail.com>
2026-03-21 14:51:08 +0200

    fully define appuser-photos relationship

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,15 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API.Entities
 {
+    [Table("Photos")]
     public class Photo
     {
         public int Id { get; set; }
         public string Url { get; set; }
         public bool IsMain { get; set; }
         public string PublicId { get; set; } // for photo storage
+        public AppUser AppUser{ get; set; } // fully define relation with the app-user
+        public int AppUserId{ get; set; }
     }
 }
