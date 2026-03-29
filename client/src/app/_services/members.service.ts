d6e2b61f3ca788331225004e0844b8fd04fa7367
YahiaHelal <yahiarashwan348@gmail.com>
2026-03-29 19:45:19 +0200

    feat: store client state on MembersService to reduce API calls

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,24 +2,43 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class MembersService {
-  private baseUrl = environment.apiUrl
+export class MembersService { // can work as a state store since it's singleton, instead of using redux
+  private baseUrl = environment.apiUrl;
+  members: Member[] = [];
+
   constructor(private httpClient: HttpClient) { }
 
   getMembers() {
-    return this.httpClient.get<Member[]>(this.baseUrl + 'users')
+    if(this.members.length > 0) {
+      return of(this.members);
+    }
+    return this.httpClient.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    )
   }
 
   getMember(username: string) {
+    const member = this.members.find(mem => mem.username === username);
+    if(member !== undefined) return of(member);
     return this.httpClient.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member) {
-    return this.httpClient.put(this.baseUrl + 'users', member)
+    return this.httpClient.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const idx = this.members.indexOf(member);
+        this.members[idx] = member;
+      })
+    )
   }
 }
