30ab7c9521bb5e33598d128fedb1f1dde5c413d3
YahiaHelal <yahiarashwan348@gmail.com>
2026-03-29 18:08:17 +0200

    feat: loading indicator and interceptro

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -20,6 +20,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
     HomeComponent,
     RegisterComponent,
     MemberListComponent,
-  MemberDetailComponent,
+    MemberDetailComponent,
     ExploreComponent,
     MessagesComponent,
     TestErrorsComponent,
@@ -43,11 +45,13 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true}
+
   ],
   bootstrap: [AppComponent]
 })
