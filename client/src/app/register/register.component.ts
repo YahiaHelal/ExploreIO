1abb88950da5e51b07869a4a9050b577eb6ebdfb
YahiaHelal <yahiarashwan348@gmail.com>
2026-04-02 02:26:47 +0200

    refactor(client): replace ngForms with reactive forms

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,20 +11,31 @@ import { ToastrService } from 'ngx-toastr';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    });
   }
 
   register() {
-    this.accountService.register(this.model).subscribe(resp => {
-      console.log(resp)
-      this.cancel();
-    }, err => {
-      console.log(err)
-      this.toastr.error(err.error.message);
-    })
+    // this.accountService.register(this.model).subscribe(resp => {
+    //   console.log(resp)
+    //   this.cancel();
+    // }, err => {
+    //   console.log(err)
+    //   this.toastr.error(err.error.message);
+    // })
+    console.log(this.registerForm.value);
   }
 
   cancel() {
