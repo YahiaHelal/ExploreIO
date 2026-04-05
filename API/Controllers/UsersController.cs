545ea1eb8cfbbd01fc136a5d45d74badd562a8bf
YahiaHelal <yahiarashwan348@gmail.com>
2026-04-05 17:58:02 +0200

    feat(api): execlude logged in user from members page

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -25,6 +25,8 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsersAsync([FromQuery] UserParams userParams)
         {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+            userParams.CurrentUsername = user.UserName;
             var users = await _userRepository.GetMembersAsync(userParams);
             Response.AddPaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages);
             return Ok(users);
