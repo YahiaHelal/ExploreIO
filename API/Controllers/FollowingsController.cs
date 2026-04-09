15d3d8ca84bcc246504459897fb05c020c724666
YahiaHelal <yahiarashwan348@gmail.com>
2026-04-09 04:22:44 +0200

    feat(api): followings controller

---
+++ API/Controllers/FollowingsController.cs
@@ -0,0 +1,56 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class FollowingsController: BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IFollowingsRepository _followingsRepository;
+        public FollowingsController(IUserRepository userRepository, IFollowingsRepository followingsRepository)
+        {
+            _followingsRepository = followingsRepository;
+            _userRepository = userRepository;
+        }
+
+        [HttpPost("{username}")]
+        public async Task<ActionResult> AddFollow(string username)
+        {
+            var sourceUserId = User.GetUserId();
+            var sourceUser = await _followingsRepository.GetUserWithFollowings(sourceUserId);
+            var followedUser = await _userRepository.GetUserByUsernameAsync(username);
+            
+            if(followedUser == null) return NotFound();
+            if(sourceUser.UserName == username) return BadRequest("You cannot follow yourself");
+
+            var userFollow = await _followingsRepository.GetUserFollow(sourceUserId, followedUser.Id);
+
+            if(userFollow != null) return BadRequest("You're already following that user");
+
+            userFollow = new UserFollow
+            {
+                SourceUserId = sourceUserId,
+                FollowedUserId = followedUser.Id
+            };
+
+            sourceUser.FollowedUsers.Add(userFollow);
+            if(await _userRepository.SaveAllAsync()) return Ok(); // should save from followings repo ?
+            
+            return BadRequest("Falied to follow user");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FollowDto>>> GetUserFollowings(string predicate)
+        {
+            var users = await _followingsRepository.GetUserFollowings(predicate, User.GetUserId());
+            return Ok(users);
+        }
+
+        //TODO: implement unfollow option
+    }
+}
