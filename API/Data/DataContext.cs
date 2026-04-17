62d2f4b67fcb5ee855d1923f93d213ea92b85d4a
YahiaHelal <yahiarashwan348@gmail.com>
2026-04-17 17:36:44 +0200

    config(db): user roles using identity

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     //NOTE: this is the ORM dealing with the DB
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<
+        AppUser, AppRole, int,
+        IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>,
+        IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
         // passing some options in the startup class to add it to the DI Container
         public DataContext(DbContextOptions options) : base(options)
@@ -18,13 +23,24 @@ namespace API.Data
         protected DataContext()
         {
         }
-        public DbSet<AppUser> Users {get;set;}
         public DbSet<UserFollow> Followings { get; set; }
 
         public DbSet<Message> Messages { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<AppUser>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(u => u.User)
+                .HasForeignKey(ur => ur.UserId)
+                .IsRequired();
+            
+            builder.Entity<AppRole>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(u => u.Role)
+                .HasForeignKey(ur => ur.RoleId)
+                .IsRequired();
 
             builder.Entity<UserFollow>()
                 .HasKey(k => new {k.SourceUserId, k.FollowedUserId});
