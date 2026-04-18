5438d5489d45c5ce5f8b01c2838cb14cdbb15d27
YahiaHelal <yahiarashwan348@gmail.com>
2026-04-18 22:01:22 +0200

    feat(api): admin edit roles functionality

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -33,6 +33,28 @@ namespace API.Controllers
             return Ok(users);
         }
 
+        [Authorize("RequireAdminRole")]
+        [HttpPost("edit-roles/{username}")]
+        public async Task<ActionResult> EditRoles(string username, [FromQuery] string roles)
+        {
+            var selectedRoles = roles.Split(",").ToArray();
+            var user = await _userManager.FindByNameAsync(username);
+
+            if(user == null) return NotFound("Couldn't find the specified user");
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var res = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+            if(!res.Succeeded) return BadRequest("Failed adding to roles");
+
+            res = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+            if(!res.Succeeded) return BadRequest("Failed removing roles");
+
+            return Ok(await _userManager.GetRolesAsync(user));
+        }
+
         [Authorize(Policy = "ModeratePhotoRole")]
         [HttpGet("photos-to-moderate")]
         public ActionResult GetPhotosForModeration()
