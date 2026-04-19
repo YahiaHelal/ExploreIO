a71f9bb784412b10ea9baebd4d1b3812766ab2cd
YahiaHelal <yahiarashwan348@gmail.com>
2026-04-19 17:50:50 +0200

    feat(client): edit roles component for admin

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -60,11 +60,11 @@ namespace API.Controllers
             var user = await _userManager.Users
             .Include(u => u.Photos)
             .SingleOrDefaultAsync(u => !string.IsNullOrEmpty(loginDto.Username) && u.UserName == loginDto.Username.ToLower());
-            if(user == null) return Unauthorized("Invalid Username");
+            if(user == null) return Unauthorized("Invalid Credintials");
             
             var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
 
-            if(!result.Succeeded) return Unauthorized();
+            if(!result.Succeeded) return Unauthorized("Invalid Credintials");
 
             return new UserDto()
             {
