f7d9158160b9646ce191bbb52d4bcfab874c1135
YahiaHelal <yahiarashwan348@gmail.com>
2026-04-20 16:57:44 +0200

    feat(api/network): add in-memory singleton presence tracker to track user status

--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -7,15 +7,29 @@ namespace API.SignalR
     [Authorize]
     public class PresenceHub: Hub
     {
+        private readonly PresenceTracker _tracker;
+        public PresenceHub(PresenceTracker tracker)
+        {
+            _tracker = tracker;
+            
+        }
         public override async Task OnConnectedAsync()
         {
+            await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
             await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername()); // web sockets does not send headers, rather query strings
+
+            var currUsers = await _tracker.GetOnlineUsers();
+            await Clients.All.SendCoreAsync("GetOnlineUsers", currUsers);
         }
 
         public override async Task OnDisconnectedAsync(Exception ex)
         {
+            await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
             await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
             
+            var currUsers = await _tracker.GetOnlineUsers();
+            await Clients.All.SendCoreAsync("GetOnlineUsers", currUsers);
+
             await base.OnDisconnectedAsync(ex);
         }
     }
