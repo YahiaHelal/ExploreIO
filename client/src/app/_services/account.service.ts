7265b463c5350c09b055376d1ca02dc9407a46fc
YahiaHelal <yahiarashwan348@gmail.com>
2026-04-20 03:44:51 +0200

    feat(client): startup online/offline notification service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -4,6 +4,7 @@ import {map} from 'rxjs/operators'
 import { User } from '../_models/user';
 import { ReplaySubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { PresenceService } from './presence.service';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,7 @@ export class AccountService {
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private presence: PresenceService) { }
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'account/login', model).pipe(
@@ -23,6 +24,7 @@ export class AccountService {
         const user = resp;
         if(user) {
           this.setCurrentUser(user);
+          this.presence.createHubConnection(user);
         }
       })
     )
@@ -31,6 +33,7 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+    this.presence.stopHubConnection();
   }
 
   setCurrentUser(user: User) {
@@ -46,6 +49,7 @@ export class AccountService {
       map((user: User) => {
         if(user) {
          this.setCurrentUser(user);
+         this.presence.createHubConnection(user);
         }
       })
     )
