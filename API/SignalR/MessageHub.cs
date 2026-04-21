c97f54f1326d4fb755281a7f2dcae68642a8bdfe
YahiaHelal <yahiarashwan348@gmail.com>
2026-04-21 22:21:25 +0200

    feat(api): notify online users when they receive a new message

--- API/SignalR/MessageHub.cs
+++ API/SignalR/MessageHub.cs
@@ -12,8 +12,13 @@ namespace API.SignalR
         private readonly IMessageRepository _messageRepository;
         private readonly IMapper _mapper;
         private readonly IUserRepository _userRepository;
-        public MessageHub(IMessageRepository messageRepository, IUserRepository userRepository, IMapper mapper)
+        private readonly IHubContext<PresenceHub> _presenceHub;
+        private readonly PresenceTracker _tracker;
+        public MessageHub(IMessageRepository messageRepository, IUserRepository userRepository, 
+            IMapper mapper, IHubContext<PresenceHub> presenceHub, PresenceTracker tracker)
         {
+            _tracker = tracker;
+            _presenceHub = presenceHub;
             _userRepository = userRepository;
             _mapper = mapper;
             _messageRepository = messageRepository;
@@ -65,6 +70,14 @@ namespace API.SignalR
             if(g.Connections.Any(c => c.Username == recipient.UserName))
             {
                 msg.DateRead = DateTime.UtcNow; // different timezones bug
+            }else
+            {
+                var conns = await _tracker.GetConnectionsForUser(recipient.UserName);
+                if(conns != null) // recipient is online but not connected to the same group
+                {
+                    await _presenceHub.Clients.Clients(conns).SendAsync("NewMessageReceived",
+                    new {username = sender.UserName, knownAs = sender.KnownAs});
+                }
             }
 
             _messageRepository.AddMessage(msg);
