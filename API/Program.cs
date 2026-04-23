9c9bfebaed7abc8a176762d5158d0be978061f9c
YahiaHelal <yahiarashwan348@gmail.com>
2026-04-23 19:13:46 +0200

    replace readme to root

--- API/Program.cs
+++ API/Program.cs
@@ -17,7 +17,7 @@ namespace API
                 var context = services.GetRequiredService<DataContext>();
                 var userManager = services.GetRequiredService<UserManager<AppUser>>();
                 var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
-                await context.Database.MigrateAsync();
+                await context.Database.MigrateAsync(); 
                 await Seed.SeedUsersAsync(userManager, roleManager);
             }catch(Exception ex)
             {
